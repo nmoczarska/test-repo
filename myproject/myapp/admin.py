e5d42963911a412a33599979aeaeac4cbd84f630
Natalia <nmoczarska@st.swps.edu.pl>
2024-11-15 13:21:58 +0100

    koniec lab04

--- myproject/myapp/admin.py
+++ myproject/myapp/admin.py
@@ -1,6 +1,15 @@
 from django.contrib import admin
 
-from .models import Team, Person
+from .models import Team, Person, Osoba, Stanowisko 
+
+class OsobaAdmin(admin.ModelAdmin):
+    list_display = ['name', 'nazwisko']
+    readonly_fields = ['data_dodania']
+
+class StanowiskoAdmin(admin.ModelAdmin):
+    list_display = ['nazwa']
 
 admin.site.register(Team)
 admin.site.register(Person)
+admin.site.register(Osoba, OsobaAdmin)
+admin.site.register(Stanowisko, StanowiskoAdmin)
--- myproject/myapp/models.py
+++ myproject/myapp/models.py
@@ -3,6 +3,8 @@ from django.db import models
 # deklaracja statycznej listy wyboru do wykorzystania w klasie modelu
 MONTHS = models.IntegerChoices('Miesiace', 'Styczeń Luty Marzec Kwiecień Maj Czerwiec Lipiec Sierpień Wrzesień Październik Listopad Grudzień')
 
+PLEC = models.IntegerChoices('Płeć', 'kobieta mężczyzna inne')
+
 SHIRT_SIZES = (
         ('S', 'Small'),
         ('M', 'Medium'),
@@ -29,3 +31,18 @@ class Person(models.Model):
 
     def __str__(self):
         return self.name
+
+class Stanowisko(models.Model):
+    nazwa = models.CharField(max_length=50)
+    opis = models.TextField(null=True, blank=True)
+    class Meta:
+        verbose_name_plural = 'Stanowiska'
+
+class Osoba(models.Model):
+    name = models.CharField(max_length=60, blank =False)
+    nazwisko = models.CharField(max_length=60, blank =False)
+    plec = models.IntegerField(choices=PLEC.choices, default=PLEC.choices[0][0])
+    stanowisko = models.ForeignKey(Stanowisko, null=True, blank=True, on_delete=models.SET_NULL)
+    data_dodania = models.DateField(auto_now_add=True)
+    class Meta:
+        verbose_name_plural = 'Osoby'
